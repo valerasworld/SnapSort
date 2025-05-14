ad973c960c158946bba5ebd187a5eed8ababe42a
Pasquale Piserchia <piserchia.pasquale@outlook.it>
2025-05-14 16:34:06 +0200

    necessary commit

--- SaveInfo/Views/DetailedViews/DetailUrlItemView.swift
+++ SaveInfo/Views/DetailedViews/DetailUrlItemView.swift
@@ -1,22 +1,32 @@
 //
-//  SwiftUIView.swift
+//  DetailUrlItemView.swift
 //  SaveInfo
 //
-//  Created by Pasquale Piserchia on 09/05/25.
+//  Created by Pasquale Piserchia on 14/05/25.
+//
+//
+//  DetailedUrlItemView.swift
+//  SaveInfo
+//
+//  Created by Pasquale Piserchia on 14/05/25.
 //
 
 import SwiftUI
 
-struct DetailedPhotoItemView: View {
+struct DetailUrlItemView: View {
     let infoObject: InfoObject
     
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                Image("image1")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxWidth: .infinity)
+                
+                Text(infoObject.stringURL ?? "")
+                    .font(.title3)
+                    .background(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.black, lineWidth: 1)
+                        .fill(infoObject.category.color.opacity(0.2)))
+                
+                    .padding(.top)
                 
                 Text(infoObject.title ?? "")
                     .foregroundStyle(infoObject.category.color)
@@ -24,11 +34,9 @@ struct DetailedPhotoItemView: View {
                     .bold()
                 
                 Text(infoObject.description ?? "")
-                    .foregroundStyle(.black)
-                    .font(.body)
-                
+                    .font(.title3)
+               
                 TagsView(infoObject: infoObject)
-                    
                 
             }
             .padding(.horizontal)
@@ -39,13 +47,13 @@ struct DetailedPhotoItemView: View {
 #Preview {
     @Previewable
     @State var infoObject = InfoObject(
-        title: "Among Us",
+        title: "Nike Air Force",
         description: "Lore ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor quam id massa faucibus dignissim. Nullam eget metus id nisl malesuada condimentum. Nam viverra fringilla erat, ut fermentum nunc feugiat eu.",
-        image: UIImage(contentsOfFile: "image1"),
-        tags: ["Multiplayer", "Party game"],
-        category: .electronics,
+        stringURL: "https://www.zalando.it/nike-sportswear-air-force-1-sneakers-basse-whiteblack-ni112o0h3-a11.html",
+        tags: ["Clothes", "Party game", "Vintage"],
+        category: .clothes,
         dateAdded: Calendar.current.date(from: DateComponents(year: 2025, month: 5, day: 1))!
     )
     
-    DetailedPhotoItemView(infoObject: infoObject)
+    DetailUrlItemView(infoObject: infoObject)
 }
