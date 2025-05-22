1cd957b48fe0b008f269a128dbab48f826645b0e
Valerii Zazulin <zazulinvalerii@gmail.com>
2025-05-22 22:29:15 +0200

    Add Resizable custom segmented control

--- Shared/Views/DashboardView/DashboardView.swift
+++ Shared/Views/DashboardView/DashboardView.swift
@@ -7,15 +7,96 @@
 
 import SwiftUI
 
+let types: [String] = ["All", "Links", "Images"]
+
 struct DashboardView: View {
     
     @State var userData = UserData()
     @State var searchText: String = ""
     
+    @State private var selectedType: String = types.first!
+    @Namespace private var animation
+    
     var body: some View {
         
         NavigationStack {
-            ScrollView {
+            ResizableHeaderScrollView {
+//                HStack(spacing: 12) {
+//                    Button {
+//                        
+//                    } label: {
+//                        Image(systemName: "line.horizontal.3")
+//                            .font(.title3)
+//                    }
+//                    
+//                    Spacer(minLength: 0)
+//                    
+////                    Button {
+////                        
+////                    } label: {
+////                        Image(systemName: "magnifyingglass")
+////                            .font(.title3)
+////                    }
+//                    
+//                    Button {
+//                        
+//                    } label: {
+//                        Image(systemName: "plus")
+//                            .font(.title3)
+//                    }
+//                        
+//                }
+//                .overlay {
+//                    Text("SnapSort")
+//                        .fontWeight(.semibold)
+//                }
+//                .foregroundStyle(.primary)
+//                .padding(.horizontal, 15)
+//                .padding(.top, 15)
+            
+            } stickyHeader: {
+                HStack(spacing: 10) {
+                    ForEach(types, id: \.self) { type in
+                        Text(type)
+                            .foregroundStyle(selectedType == type ? .white : .gray)
+                            .cornerRadius(10)
+                            .fontWeight(selectedType == type ? .semibold : .regular) // ??
+                            .padding(.horizontal, 20)
+                            .frame(height: 30)
+                            .frame(maxWidth: .infinity)
+                            .background {
+                                if selectedType == type {
+                                    Capsule()
+                                        .fill(.blue.gradient)
+                                        .matchedGeometryEffect(id: "ACTIVETAB", in: animation)
+                                }
+                            }
+                            .contentShape(.rect)
+                            .onTapGesture {
+                                withAnimation(.snappy/*(duration: 0.3)*/) {
+                                    selectedType = type
+                                }
+                            }
+                    }
+                }
+                
+                .foregroundStyle(.primary)
+//                .safeAreaPadding(15)
+                .background {
+                    Capsule()
+                        .fill(.black.gradient.opacity(0.07))
+//                        .safeAreaPadding(10)
+                }
+                .padding(.top, -10)
+                .padding(.bottom, 12)
+                .padding(.horizontal, 16)
+            } background: {
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+                    .overlay(alignment: .bottom) {
+                        Divider()
+                    }
+            } content: {
                 LazyVStack(alignment: .leading) {
                     Group {
                         RecentlyAddedSectionHeaderView(userData: userData)
@@ -31,9 +112,21 @@ struct DashboardView: View {
                     }
                 }
             }
-            .navigationTitle("Dashboard")
-            .searchable(text: $searchText)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("SnapSort")
+            .toolbarBackgroundVisibility(.hidden, for: .navigationBar)
+            
+            
+            .searchable(text: $searchText, placement: .navigationBarDrawer)
             .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.horizontal.3")
+                            .foregroundStyle(.black)
+                    }
+                }
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
                         
---
+++ Shared/Views/DashboardView/ResizableHeaderScrollView.swift
@@ -0,0 +1,93 @@
+//
+//  ResizableHeaderScrollView.swift
+//  SaveInfo
+//
+//  Created by Valery Zazulin on 22/05/25.
+//
+
+import SwiftUI
+
+struct ResizableHeaderScrollView<Header: View, StickyHeader: View, Background: View, Content: View>: View {
+    
+    var spacing: CGFloat = 10
+    @ViewBuilder var header: Header
+    @ViewBuilder var stickyHeader: StickyHeader
+    // Only for header background, not for the entire View
+    @ViewBuilder var background: Background
+    @ViewBuilder var content: Content
+    
+    // View Properties:
+    @State var currentDragOffset: CGFloat = 0
+    @State var previousDragOffset: CGFloat = 0
+    @State var headerOffset: CGFloat = 0
+    @State var headerSize: CGFloat = 0
+    @State var scrollOffset: CGFloat = 0
+    
+    var body: some View {
+        ScrollView(.vertical) {
+            content
+        }
+        .frame(maxWidth: .infinity)
+        .onScrollGeometryChange(for: CGFloat.self, of: {
+            $0.contentOffset.y + $0.contentInsets.top
+        }, action: { _, newValue in
+            scrollOffset = newValue
+        })
+        .simultaneousGesture(
+            DragGesture(minimumDistance: 10)
+                .onChanged({ value in
+                    // Adjusting the minimum distance value
+                    // Thus it starts from 0.
+                    let dragOffset = -max(0, abs(value.translation.height) - 50) * (value.translation.height < 0 ? -1 : 1)
+                    
+                    previousDragOffset = currentDragOffset
+                    currentDragOffset = dragOffset
+                    
+                    let deltaOffset = (currentDragOffset - previousDragOffset).rounded()
+                    
+                    headerOffset = max(min(headerOffset + deltaOffset, headerSize), 0)
+                }).onEnded({ _ in
+                    withAnimation(.easeInOut(duration: 0.2)) {
+                        if headerOffset > (headerSize * 0.5) && scrollOffset > headerSize {
+                            headerOffset = headerSize
+                        } else {
+                            headerOffset = 0
+                        }
+                    }
+                    
+                    // Resetting offset data
+                    currentDragOffset = 0
+                    
+                })
+        )
+        .safeAreaInset(edge: .top, spacing: spacing) {
+            CombinedHeaderView()
+        }
+    }
+    
+    @ViewBuilder
+    private func CombinedHeaderView() -> some View {
+        VStack(spacing: spacing) {
+            header
+                .onGeometryChange(for: CGFloat.self) {
+                    $0.size.height
+                } action: { newValue in
+                    // Optional "spacing"
+                    headerSize = newValue/* + spacing*/
+                }
+
+            stickyHeader
+        }
+        .offset(y: -headerOffset)
+        .clipped()
+        .background {
+            background
+                .ignoresSafeArea()
+        }
+        
+    }
+}
+
+//#Preview {
+//    ResizableHeaderScrollView()
+//}
