a6245777ee4e54411d67fa2797d489a65b25b8f4
Pasquale Piserchia <piserchia.pasquale@outlook.it>
2025-06-12 07:05:20 +0200

    Reorder and add icons for new Category picker

--- SaveInfo/Views/CreateCategorySheetView.swift
+++ SaveInfo/Views/CreateCategorySheetView.swift
@@ -288,6 +288,37 @@ struct IconPickerView: View {
         "film.fill",
         "tv.fill",
         
+        // Music / Audio
+        "music.note.list",
+        "music.mic",
+        "headphones",
+        "guitars.fill",
+        
+        // Wellness / Fitness / Health
+        "figure.mind.and.body",
+        "heart.fill",
+        "bolt.heart.fill",
+        "cross.case.fill",
+        
+        // Work / Productivity
+        "briefcase.fill",
+        "calendar",
+        "clock.badge.checkmark",
+        "chart.bar.xaxis",
+        
+        // Electronics / Tech / Gadgets
+        "ipad.and.iphone",
+        "desktopcomputer",
+        "macbook.and.iphone",
+        "gamecontroller.fill",
+        
+        // Restaurants / Food / Cooking
+        "fork.knife",
+        "takeoutbag.and.cup.and.straw.fill",
+        "cup.and.saucer.fill",
+        "birthday.cake.fill",
+        "wineglass.fill",
+        
         // Books / Reading / Learning
         "books.vertical.fill",
         "text.book.closed.fill",
@@ -306,22 +337,30 @@ struct IconPickerView: View {
         "tennisball.fill",
         "soccerball.circle.fill.inverse",
         "basketball.fill",
+        "figure.run",
+        "figure.open.water.swim",
         
         // Memes / Fun / Entertainment
         "gift.fill",
-        "face.dashed.fill",
+        "face.smiling.inverse",
         "theatermasks.fill",
         "sparkles",
         "figure.stand.line.dotted.figure.stand",
         
         // Travel / Places / Outdoors
+        "house.fill",
         "airplane",
         "car.fill",
+        "bicycle",
+        "map.fill",
+        "location.fill",
         "mappin.and.ellipse",
+        "binoculars.fill",
         "globe.americas.fill",
         "tent.fill",
         
         // Goods / Shopping / Products
+        "tshirt.fill",
         "bag.fill",
         "cart.fill",
         "shippingbox.fill",
@@ -343,14 +382,21 @@ struct IconPickerView: View {
         "ellipsis.circle.fill",
         "questionmark.folder.fill",
         
-        
+
         // Classic symbol for pets
         "pawprint.fill",
         "tortoise.fill",
         "hare.fill",
         "ant.fill",
         "ladybug.fill",
+        "camera.macro",
+        "rainbow",
+        "tree.fill",
         "leaf.fill",
+        "sun.max.fill",
+        "moon.fill",
+        "cloud.fill",
+        "snowflake",
         "bird.fill",
         "fish.fill",
         "pawprint.fill",
